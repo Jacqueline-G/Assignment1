cb732ac5544f0c98671d0910cdf9b7ce88439ebe
Jacqueline-G <garruto.j@husky.neu.edu>
2016-01-23 16:52:40 -0500

    First Draft
    Just putting up my first round of this one, created a grid of squares.
    Struggled with creating the while loop- kept crashing the program.

--- sketch.js
+++ sketch.js
@@ -18,9 +18,26 @@
 */
 
 function setup() {
+  createCanvas(600,400);
   
+  var m = 10
+  
+  while( m > 0){
+    print("Hello" + m);
+    m = m - 1;
+  }
 }
 
 function draw() {
-  
+  background (0);
+  strokeWeight(4);
+  stroke(255);
+
+  for (var x = 0; x <= width; x += 50) {
+    for (var y = 0; y <= height; y += 50) {
+    fill(0, 255, 0);
+    rect(x, y, 25, 25);
+    } 
+}
+
 }
